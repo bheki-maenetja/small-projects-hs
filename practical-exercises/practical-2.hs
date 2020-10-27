2f2b9440db9a3b56a2be5dcf3c84a2ce611bbd79
bheki-maenetja <bhekimaenetja@gmail.com>
2020-10-27 14:18:04 +0000

    adding recursive solutions

--- practical-exercises/practical-2.hs
+++ practical-exercises/practical-2.hs
@@ -3,6 +3,12 @@
 sumsq::Int->Int
 sumsq n = sum[i^2 | i <- [1..n]]
 
+-- recursive solution
+r_sumsq::Int->Int
+r_sumsq n
+    | n == 0 = 0
+    | otherwise = n^2 + r_sumsq (n-1)
+
 -- 2. Define a function prodsum that takes a positive Int value n and returns the product of positive odd integers not exceeding n and the sum of the positive even integers not exceeding n multiplied together
 prodsum::Int->Int
 prodsum n = product[i | i <- [1..n], i `mod` 2 /= 0]  * sum[i | i <- [0..n], i `mod` 2 == 0]
