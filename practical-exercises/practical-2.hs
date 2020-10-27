2b0e970c5169618064ca93f635b88ebae474436b
bheki-maenetja <bhekimaenetja@gmail.com>
2020-10-27 14:35:38 +0000

    added alternative solution

--- practical-exercises/practical-2.hs
+++ practical-exercises/practical-2.hs
@@ -3,8 +3,7 @@
 sumsq::Int->Int
 sumsq n = sum[i^2 | i <- [1..n]]
 
--- recursive solution
-r_sumsq::Int->Int
+r_sumsq::Int->Int -- recursive solution
 r_sumsq n
     | n == 0 = 0
     | otherwise = n^2 + r_sumsq (n-1)
@@ -12,6 +11,21 @@ r_sumsq n
 -- 2. Define a function prodsum that takes a positive Int value n and returns the product of positive odd integers not exceeding n and the sum of the positive even integers not exceeding n multiplied together
 prodsum::Int->Int
 prodsum n = product[i | i <- [1..n], i `mod` 2 /= 0]  * sum[i | i <- [0..n], i `mod` 2 == 0]
+
+prodOddNums::Int->Int -- product of all odd number up to n
+prodOddNums n
+    | n == 0 = 1
+    | n `mod` 2 /= 0 = n * prodOddNums (n-1)
+    | otherwise = 1 * prodOddNums(n-1)
+
+addEvenNums::Int->Int -- sum of all even numbers up to n
+addEvenNums n
+    | n == 0 = 0
+    | n `mod` 2 == 0 = n + addEvenNums (n-1)
+    | otherwise = 0 + addEvenNums(n-1)
+
+new_prodsum::Int->Int -- alternative solution
+new_prodsum n = prodOddNums n * addEvenNums n
 
 -- 3. Define a function isitprime that takes a positive Int value n and returns True if n is a prime number and False if it is not
 isitprime::Int->Bool
