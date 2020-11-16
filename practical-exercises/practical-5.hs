a15bd57f371da87aa778b7cc00336f8580212f1f
bheki-maenetja <bhekimaenetja@gmail.com>
2020-11-16 12:46:10 +0000

    completed second part of exercise 5

--- practical-exercises/practical-5.hs
+++ practical-exercises/practical-5.hs
@@ -3,3 +3,12 @@
 insertAt::Int->[Int]->Int->[Int]
 insertAt x [] _ = [x]
 insertAt n xs i = take (i-1) xs ++ [n] ++ drop (i-1) xs
+
+-- 2. Define a recursive function that merges two sorted lists of values to give a single sorted list
+merge::[Int]->[Int]->[Int]
+merge [] [] = []
+merge xs [] = xs
+merge [] xs = xs
+merge (x:xs) (y:ys)
+    | x <= y = [x] ++ merge xs (y:ys)
+    | otherwise = [y] ++ merge (x:xs) ys
