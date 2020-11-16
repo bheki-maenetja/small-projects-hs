745becdc84087bad9b821874a01fe3963504b899
bheki-maenetja <bhekimaenetja@gmail.com>
2020-11-16 13:24:03 +0000

    finished part 3 of exercise 5 sort of

--- practical-exercises/practical-5.hs
+++ practical-exercises/practical-5.hs
@@ -12,3 +12,13 @@ merge [] xs = xs
 merge (x:xs) (y:ys)
     | x <= y = [x] ++ merge xs (y:ys)
     | otherwise = [y] ++ merge (x:xs) ys
+
+-- 3. Define a recursive function msort that implements merge sort, which can be specified by the following two rules:
+-- (1) Lists of length smaller than or equal to 1 are already sorted;
+-- (2) Other lists can be sorted by sorting the two halves and merging the resulting list
+
+msort::[Int]->[Int]
+msort [] = []
+msort xs
+    | length xs == 2 = merge (take 1 xs) (drop 1 xs)
+    | otherwise = merge (msort (take (length xs `div` 2) xs))  (msort (drop (length xs `div` 2) xs))
