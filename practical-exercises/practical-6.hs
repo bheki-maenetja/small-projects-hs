a161ecdcc5ce780fc61adc98abdab2bc5034ae5c
bheki-maenetja <bhekimaenetja@gmail.com>
2020-11-23 16:41:36 +0000

    completed practical exercise 6

--- practical-exercises/practical-6.hs
+++ practical-exercises/practical-6.hs
@@ -1,5 +1,4 @@
 -- PRACTICAL 6
-
 -- 1. Define the following library functions using recursion:
 -- a) Decide if all logical values in a list are true.
 and2::[Bool]->Bool
@@ -26,4 +25,15 @@ select (x:xs) n
     | n <= 1 = x
     | n > length xs = head (reverse xs)
     | otherwise = select xs (n-1)
+
+-- 2. A triple (x, y, z) of positive integers is called Pythagorean if x^2 + y^2 = z^2. Using a list comprehension, define a function that maps an integer n to all such triples with components in [1..n]
+pyths::Int->[(Int,Int,Int)]
+pyths n = [(a,b,n) | a <- [1..n], b <- [1..n], a*a + b*b == n*n ]
+
+-- 3. Define a function groups :: Int -> [Int] -> [[Int]] that given a number k and a list, chops the list up into sublists of length k (the length of the last element of the resulting list might be less than k)
+groups::Int->[Int]->[[Int]]
+groups _ [] = []
+groups n xs
+    | n < 1 = [xs]
+    | otherwise = (take n xs):[] ++ groups n (drop n xs)
 
