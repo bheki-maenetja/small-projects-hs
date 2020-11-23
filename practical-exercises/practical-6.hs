2ee36baccf7f5601c6243697ac0181f67ca65189
bheki-maenetja <bhekimaenetja@gmail.com>
2020-11-23 15:56:59 +0000

    new exercise

---
+++ practical-exercises/practical-6.hs
@@ -0,0 +1,10 @@
+-- PRACTICAL 6
+
+-- 1. Define the following library functions using recursion:
+-- a) Decide if all logical values in a list are true.
+and2:: [Bool] -> Bool
+
+
+-- b) Concatenate a list of lists. concat2:: [[Int]] -> [Int]
+-- c) Produce a list with n identical elements. replicate2:: Int -> Int -> [Int]
+-- d) Select the nth element of a list. select:: [Int] -> Int -> Int
