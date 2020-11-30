8fb10bc59a7a39e7706dc10cd01e7d7bab86df17
bheki-maenetja <bhekimaenetja@gmail.com>
2020-11-30 19:46:30 +0000

    completed practical exercise 7

--- practical-exercises/practical-7.hs
+++ practical-exercises/practical-7.hs
@@ -2,12 +2,12 @@
 -- 1. Consider a polymorphic function dropEvery that drops every n'th element from a list
 -- a) list comprehension
 dropEvery::[a]->Int->[a]
--- dropEvery xs i = [ xs!!(n-1)  | n<-[1..(length xs)], n `mod` i /= 0]
+dropEvery xs i = [ xs!!(n-1)  | n<-[1..(length xs)], n `mod` i /= 0]
 
 -- b) guarded equations
--- dropEvery xs i 
---     | length xs < i = xs
---     | otherwise = take (i-1) xs ++ dropEvery (drop (i) xs) i
+dropEvery xs i 
+    | length xs < i = xs
+    | otherwise = take (i-1) xs ++ dropEvery (drop (i) xs) i
 
 -- c) pattern matching
 dropEvery [] _ = []
