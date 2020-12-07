4fb0a1d8ecc0775dbce66d249574654e0ba5dc1c
bheki-maenetja <bhekimaenetja@gmail.com>
2020-12-07 19:34:59 +0000

    completing second of coursework

--- practical-exercises/cw.hs
+++ practical-exercises/cw.hs
@@ -10,3 +10,11 @@ makeStep l r = ['*' | n <- [1..l]] ++ "\n" ++ makeStep l (r-1)
 steps::Int->Int->Int->[Char] -- function that takes three positive Int values m n p and returns a String that can be displayed as p steps, of height m and width n, the right way up, and repeats the pattern in opposite way
 steps _ _ 0 = []
 steps m n p = concat ([makeStep (n * x) m | x <-[1..p]] ++ reverse [makeStep (n * x) m | x <-[1..p]])
+
+-- b)
+makeFlag::Int->Int->[Char] -- function that makes one flag pattern
+makeFlag l n
+    | n == 0 = ['*' | i <-[1..l]] ++ "\n"
+    | l == n = ['*' | i <-[1..l]] ++ "\n" ++ makeFlag l (n-1)
+    | n == 1 = [] ++ makeFlag l (n-1)
+    | otherwise = [if i == 1 || i == l || i == 1+(l-n) || i == n then '*' else ' ' | i  <-[1..l]] ++ "\n" ++ makeFlag l (n-1)
